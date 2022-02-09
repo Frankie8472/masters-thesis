12448932353c24368820993e42272a1a7d9c5079
franz <wombatodes@gmail.com>
2022-02-09 12:12:35 +0100

    added create corpus function for testing seed influence

--- thesis_code/main.py
+++ thesis_code/main.py
@@ -1,11 +1,197 @@
-# Library imports
-import numpy as np
+import gc
+import json
+import os
+import sys
+
 import torch
-from transformers import pipeline, set_seed
-from transformers import GPT2LMHeadModel
-from transformers import GPT2Tokenizer
+from tqdm.auto import tqdm
+from transformers import AutoTokenizer, AutoModelForCausalLM, GPT2Tokenizer, GPT2LMHeadModel, GPTNeoForCausalLM, \
+    set_seed
 
-# Seed for reproducability
-t = GPT2Tokenizer.from_pretrained("gpt2")
-print(t.model_input_names)
 
+def create_corpus(
+        tokenizer_name="gpt2",
+        model_name="gpt2",
+        max_document_length=None,
+        device="cpu",
+        corpus_size=1,
+        tokenizer_model=AutoTokenizer,
+        lm_model=AutoModelForCausalLM,
+        pad_token_id=None,
+        save_path="data/test",
+        load_size=1
+    ):
+    r"""
+    Generates sequences/documents/a corpus for models with a language modeling head.
+
+    Parameters:
+        corpus_size (`int`, *optional*, defaults to 1):
+            The corpus size to be generated (number of documents)
+        model_name (`str`, *optional*, defaults to "openai-gpt"):
+            The model name of the pre-trained model: openai-gpt, gpt2-small, gpt2, gpt2-large, gpt2-xl, transfo-xl-wt103, EleutherAI/gpt-neo-2.7B, ctrl
+        max_document_length (`int`, *optional*, defaults to None):
+            The maximum document length, normally set to tokenizer.max_length
+        tokenizer_model (`PreTrainedTokenizer`, *optional*, defaults to AutoTokenizer):
+            The pre-trained tokenizer class
+        lm_model (`PreTrainedModel`, *optional*, defaults to AutoModelForCausalLM):
+            The pre-trained model class with language modeling head
+        device (`str`, *optional*, defaults to "cpu"):
+            The device the computations commence "cpu" or "cuda"
+    """
+
+    tokenizer = tokenizer_model.from_pretrained(tokenizer_name)
+    model = lm_model.from_pretrained(model_name)
+
+    max_document_length = max_document_length if max_document_length is not None else tokenizer.model_max_length
+    if pad_token_id is not None:
+        if pad_token_id == 'eos_token_id':
+            pad_token_id = tokenizer.eos_token_id
+        else:
+            print("ERROR: Undefinded/unimplemented pad_token_id")
+
+    model = model.to(device)
+
+    decoded_output = []
+
+    for i in tqdm(range(0, corpus_size, load_size)):
+        step_size = min(load_size, corpus_size-i)
+        encoded_output = model.generate(
+            # all parameters have to be set as otherwise the config of the pretrained model will be taken
+            input_ids=None,
+            max_length=max_document_length,
+            do_sample=True,                         # False implies Greedy search
+            early_stopping=False,
+            num_beams=1,                            # 1 deactivates beam_search
+            temperature=1.0,                        # 1.0 deactivates temperature
+            top_k=0,                                # 0 deactivates top_k sampling
+            top_p=1.0,                              # 1.0 deactivates top_p sampling
+            repetition_penalty=1.0,                 # 1.0 deactivates repetition_penalty
+            pad_token_id=pad_token_id,              # For open-end generation set to eos_token_id
+            #bos_token_id=bos_token_id,
+            #eos_token_id=eos_token_id,
+            length_penalty=1.0,                     # 1.0 deactivates length_penalty
+            no_repeat_ngram_size=0,                 # 0 deactivates no_repeat_ngram_size
+            encoder_no_repeat_ngram_size=0,         # 0 deactivates encoder_no_repeat_ngram_size
+            num_return_sequences=step_size,       # The number of independently computed returned sequences for each element in the batch. No input means batch size of one.
+            num_beam_groups=1,
+            output_scores=False,                    # Will be important if you want the prediction scores!
+        )
+
+        for j in range(step_size):
+            decoded_output.append(tokenizer.decode(encoded_output[j], skip_special_tokens=True))
+
+    print(decoded_output)
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+
+def main():
+    corpus_size = 4
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    load_size = 2
+
+    set_seed(42)    # For reproducability: set1 seed=42 set2 seed=1337
+
+    case = 1
+    if case == 1:
+        print(f"Entering case: {case}")
+        create_corpus(
+            tokenizer_name="gpt2",
+            model_name="data/model_gpt2-wiki_no_titles",
+            max_document_length=None,
+            device=device,
+            corpus_size=corpus_size,
+            tokenizer_model=GPT2Tokenizer,
+            lm_model=GPT2LMHeadModel,
+            pad_token_id='eos_token_id',
+            save_path="/cluster/home/knobelf/ma/data/data_gpt2-baseline_no_header2.json",
+            load_size=load_size
+        )
+    elif case == 2:
+        print(f"Entering case: {case}")
+        create_corpus(
+            tokenizer_name="gpt2",
+            model_name="ma/data/model_gpt2-baseline_self",
+            max_document_length=None,
+            device=device,
+            corpus_size=corpus_size,
+            tokenizer_model=GPT2Tokenizer,
+            lm_model=GPT2LMHeadModel,
+            pad_token_id='eos_token_id',
+            save_path="/cluster/home/knobelf/ma/data/data_gpt2-baseline_self.json",
+            load_size=load_size
+        )
+    elif case == 3:
+        print(f"Entering case: {case}")
+        create_corpus(
+            tokenizer_name="gpt2",
+            model_name="ma/data/model_gpt2-baseline",
+            max_document_length=None,
+            device=device,
+            corpus_size=corpus_size,
+            tokenizer_model=GPT2Tokenizer,
+            lm_model=GPT2LMHeadModel,
+            pad_token_id='eos_token_id',
+            save_path="/cluster/home/knobelf/ma/data/data_gpt2-baseline.json",
+            load_size=load_size
+        )
+    elif case == 4:
+        print(f"Entering case: {case}")
+        create_corpus(
+            tokenizer_name="gpt2",
+            model_name="gpt2",
+            max_document_length=None,
+            device=device,
+            corpus_size=corpus_size,
+            tokenizer_model=GPT2Tokenizer,
+            lm_model=GPT2LMHeadModel,
+            pad_token_id='eos_token_id',
+            save_path="/cluster/home/knobelf/ma/data/data2_gpt2.json",
+            load_size=load_size
+        )
+    elif case == 5:
+        print(f"Entering case: {case}")
+        create_corpus(
+            tokenizer_name="gpt2-large",
+            model_name="gpt2-large",
+            max_document_length=None,
+            device=device,
+            corpus_size=corpus_size,
+            tokenizer_model=GPT2Tokenizer,
+            lm_model=GPT2LMHeadModel,
+            pad_token_id='eos_token_id',
+            save_path="/cluster/home/knobelf/ma/data/data_gpt2-large.json",
+            load_size=load_size
+        )
+    elif case == 6:
+        print(f"Entering case: {case}")
+        create_corpus(
+            tokenizer_name="gpt2-xl",
+            model_name="gpt2-xl",
+            max_document_length=None,
+            device=device,
+            corpus_size=corpus_size,
+            tokenizer_model=GPT2Tokenizer,
+            lm_model=GPT2LMHeadModel,
+            pad_token_id='eos_token_id',
+            save_path="/cluster/home/knobelf/ma/data/data_gpt2-xl.json",
+            load_size=load_size
+        )
+    elif case == 7:
+        print(f"Entering case: {case}")
+        create_corpus(
+            tokenizer_name="EleutherAI/gpt-neo-2.7B",
+            model_name="EleutherAI/gpt-neo-2.7B",
+            max_document_length=2048,
+            device=device,
+            corpus_size=corpus_size,
+            tokenizer_model=GPT2Tokenizer,
+            lm_model=GPTNeoForCausalLM,
+            pad_token_id=None,
+            save_path="/cluster/home/knobelf/ma/data/data_gpt2neo.json",
+            load_size=load_size
+        )
+
+
+main()
