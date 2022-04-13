3258ec74d53bee59915744afae91239d3a65600e
franz <wombatodes@gmail.com>
2022-04-13 19:30:29 +0200

    refactored bjobs generator, added specific call functions for generate_bjobs

--- thesis_code/main.py
+++ thesis_code/main.py
@@ -1,83 +1,207 @@
+import gc
 import json
 import os
+import random
+
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 from matplotlib.font_manager import FontProperties
 from matplotlib.ticker import ScalarFormatter
 from matplotlib.pyplot import cm
-from transformers import set_seed
 
 
-def generate_bjobs():
-    topics = [2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 50, 100]
-    models = [23, 24, 25, 26]   #[1, 2, 7, 8]
-    samples = [1]     # [1, 2, 3, 4, 5]
-    for model in models:
-        for topic in topics:
-            for union in [0, 1]:
-                for i in samples:
-                    combi = "un" if union else "is"
-                    if model == 1:
-                        name = f"wiki_nt-gpt2_nt-{combi}-{topic}.txt"
-                    elif model == 2:
-                        name = f"gpt2_nt-wiki_nt-{combi}-{topic}.txt"
-                    elif model == 3:
-                        name = f"gpt2_nt_1-gpt2_nt_2-{combi}-{topic}.txt"
-                    elif model == 4:
-                        name = f"gpt2_nt_2-gpt2_nt_1-{combi}-{topic}.txt"
-                    elif model == 5:
-                        name = f"gpt2_1-gpt2_2-{combi}-{topic}.txt"
-                    elif model == 6:
-                        name = f"gpt2_2-gpt2_1-{combi}-{topic}.txt"
-                    elif model == 7:
-                        name = f"wiki_nt-arxiv-{combi}-{topic}.txt"
-                    elif model == 8:
-                        name = f"arxiv-wiki_nt-{combi}-{topic}.txt"
-                    elif model == 9:
-                        name = f"gpt2_nt-arxiv-{combi}-{topic}.txt"
-                    elif model == 10:
-                        name = f"arxiv-gpt2_nt-{combi}-{topic}.txt"
-                    elif model == 11:
-                        name = f"gpt2_nt-arxiv-top_p-{combi}-{topic}.txt"
-                    elif model == 12:
-                        name = f"arxiv-gpt2_nt-top_p-{combi}-{topic}.txt"
-                    elif model == 13:
-                        name = f"gpt2_nt-wiki_top_p-{combi}-{topic}.txt"
-                    elif model == 14:
-                        name = f"wiki-gpt2_nt-top_p-{combi}-{topic}.txt"
-                    elif model == 15:
-                        name = f"gpt2_nt_1-gpt2_nt_2-top_p-{combi}-{topic}.txt"
-                    elif model == 16:
-                        name = f"gpt2_np_2-gpt2_nt_1-top_p-{combi}-{topic}.txt"
-                    elif model == 17:
-                        name = f"gpt2_nt-arxiv-typ_p-{combi}-{topic}.txt"
-                    elif model == 18:
-                        name = f"arxiv-gpt2_nt-typ_p-{combi}-{topic}.txt"
-                    elif model == 19:
-                        name = f"gpt2_nt-wiki_typ_p-{combi}-{topic}.txt"
-                    elif model == 20:
-                        name = f"wiki-gpt2_nt-typ_p-{combi}-{topic}.txt"
-                    elif model == 21:
-                        name = f"gpt2_nt_1-gpt2_nt_2-typ_p-{combi}-{topic}.txt"
-                    elif model == 22:
-                        name = f"gpt2_nt_2-gpt2_nt_1-typ_p-{combi}-{topic}.txt"
-                    elif model == 23:
-                        name = f"gpt2_nt-gpt2-{combi}-{topic}.txt"
-                    elif model == 24:
-                        name = f"gpt2-gpt2_nt-{combi}-{topic}.txt"
-                    elif model == 25:
-                        name = f"gpt2-wiki_nt-{combi}-{topic}.txt"
-                    elif model == 26:
-                        name = f"wiki_nt-gpt2-{combi}-{topic}.txt"
-                    else:
-                        print("ERROR")
-                        return
-                    if len(samples) > 1:
-                        name = f"{name[:-4]}-{i}{name[-4:]}"
-                        print(f"bsub -N -W 24:00 -n 48 -R \"rusage[mem=2666]\" -o logs/log-{name} \"python /cluster/work/cotterell/knobelf/train_lda.py {union} {model} {topic} {i}\"")
+def generate_bjobs(topics, file_path, data_path, topic_models, models, models_copy, duals, sampling_methods, samples, combis, time="04:00", num_cpu=1, cpu_mem=1024, use_gpu=False, num_gpus=1, gpu_mem=8192, gpu_model=None, num_var=1, calc_var=False, calc_score=False, score_mode=None):
+    if calc_score and score_mode is None:
+        raise AssertionError(">> ERROR: calc_score needs score_mode not to be None")
+    models_copy_old = models_copy.copy()
+    if calc_var and num_var > 0:
+        var_range = np.arange(1, num_var + 1).astype(str)
+    else:
+        var_range = [""]
+    for var in var_range:
+        var_log = var_cmd = ""
+        if var != "":
+            var_log = f"-{var}"
+            var_cmd = f" {var}"
+        for topic_model in topic_models:
+            for dual in duals:
+                for sampling_method in sampling_methods:
+                    sampling_method_short = "" if sampling_method == "multinomial" else "-" + sampling_method
+                    for first in models:
+                        for second in models_copy:
+                            if sampling_method != "multinomial" and first != "gpt2_nt" and second != "gpt2_nt":
+                                continue
+                            for topic in topics:
+                                if topic == 1:
+                                    topic_model_short = ""
+                                else:
+                                    topic_model_short = topic_model + "-"
+                                for combi in combis:
+                                    combi_short = "is" if combi == "intersection" else "un"
+                                    if calc_score:
+                                        for score in score_mode:
+                                            print(f"bsub -N -W {time} -n {num_cpu} -R \"rusage[mem={cpu_mem}]\" -oo logs/{samples}/log-{score}-{first}-{second}{sampling_method_short}.txt \"python {file_path}score_lda.py {data_path} {score} {samples} {first}-{second}{sampling_method_short}\"")
+                                    else:
+                                        if use_gpu:
+                                            if gpu_model is None:
+                                                print(f"bsub -N -W {time} -n {num_cpu} -R \"rusage[mem={cpu_mem},ngpus_excl_p={num_gpus}]\" -R \"select[gpu_mtotal0>={gpu_mem}]\" -oo logs/{samples}/log-{topic_model_short}{first}-{second}-{combi_short}-{topic}{sampling_method_short}-{dual}{var_log}.txt \"python {file_path}train_lda.py {data_path} {topic_model} {first} {second} {dual} {sampling_method} {topic} {combi} {samples}{var_cmd}\"")
+                                            else:
+                                                print(f"bsub -N -W {time} -n {num_cpu} -R \"rusage[mem={cpu_mem},ngpus_excl_p={num_gpus}]\" -R \"select[gpu_model0=={gpu_model}]\" -oo logs/{samples}/log-{topic_model_short}{first}-{second}-{combi_short}-{topic}{sampling_method_short}-{dual}{var_log}.txt \"python {file_path}train_lda.py {data_path} {topic_model} {first} {second} {dual} {sampling_method} {topic} {combi} {samples}{var_cmd}\"")
+                                        else:
+                                            print(f"bsub -N -W {time} -n {num_cpu} -R \"rusage[mem={cpu_mem}]\" -oo logs/{samples}/log-{topic_model_short}{first}-{second}-{combi_short}-{topic}{sampling_method_short}-{dual}{var_log}.txt \"python {file_path}train_lda.py {data_path} {topic_model} {first} {second} {dual} {sampling_method} {topic} {combi} {samples}{var_cmd}\"")
+                        models_copy.remove(first)
+                    models_copy = models_copy_old.copy()
 
-                    else:
-                        print(f"bsub -N -W 24:00 -n 48 -R \"rusage[mem=2666]\" -o logs/log-{name} \"python /cluster/work/cotterell/knobelf/train_lda.py {union} {model} {topic}\"")
+
+def generate_tokenize_bjobs(samples=10000):
+    models = ["gpt2_nt", "gpt2", "trafo_xl_nt", "trafo_xl", "wiki_nt", "arxiv"]
+    if samples > 10000:
+        models.remove("trafo_xl_nt")
+        models.remove("trafo_xl")
+
+    models_copy = models.copy()
+
+    generate_bjobs(
+        topics=[1],
+        file_path="/cluster/work/cotterell/knobelf/",
+        data_path="/cluster/work/cotterell/knobelf/data/",
+        topic_models=["classic_lda"],
+        models=models,
+        models_copy=models_copy,
+        duals=["tokenization"],
+        sampling_methods=["multinomial", "top_p", "typ_p"],
+        samples=samples,
+        combis=["intersection", "union"],
+        time="24:00",
+        num_cpu=8,
+        cpu_mem=5120,
+        use_gpu=False,
+        num_gpus=1,
+        gpu_mem=30000,
+        gpu_model=None,
+        num_var=10,
+        calc_var=False
+    )
+
+
+def generate_classic_lda_bjobs(samples=10000):
+    models = ["gpt2_nt", "gpt2", "trafo_xl_nt", "trafo_xl", "wiki_nt", "arxiv"]
+    if samples > 10000:
+        models.remove("trafo_xl_nt")
+        models.remove("trafo_xl")
+
+    models_copy = models.copy()
+
+    generate_bjobs(
+        topics=[2, 3, 5, 10, 20, 50, 100],
+        file_path="/cluster/work/cotterell/knobelf/",
+        data_path="/cluster/work/cotterell/knobelf/data/",
+        topic_models=["classic_lda"],
+        models=models,
+        models_copy=models_copy,
+        duals=["first", "second"],
+        sampling_methods=["multinomial", "top_p", "typ_p"],
+        samples=samples,
+        combis=["intersection", "union"],
+        time="24:00",
+        num_cpu=4,
+        cpu_mem=5120,
+        use_gpu=False,
+        num_gpus=1,
+        gpu_mem=30000,
+        gpu_model=None,
+        num_var=10,
+        calc_var=False
+    )
+
+
+def generate_classic_lda_variation_bjobs(samples=10000):
+    generate_bjobs(
+        topics=[2, 3, 5, 10, 20, 50, 100],
+        file_path="/cluster/work/cotterell/knobelf/",
+        data_path="/cluster/work/cotterell/knobelf/data/",
+        topic_models=["classic_lda"],
+        models=["gpt2_nt"],
+        models_copy=["gpt2_nt", "wiki_nt", "arxiv"],
+        duals=["first", "second"],
+        sampling_methods=["multinomial"],
+        samples=samples,
+        combis=["intersection", "union"],
+        time="24:00",
+        num_cpu=4,
+        cpu_mem=10240,
+        use_gpu=False,
+        num_gpus=1,
+        gpu_mem=30000,
+        gpu_model=None,
+        num_var=9,
+        calc_var=True
+    )
+
+
+def generate_neural_lda_bjobs(samples=10000):
+    models = ["gpt2_nt", "gpt2", "trafo_xl_nt", "trafo_xl", "wiki_nt", "arxiv"]
+    if samples > 10000:
+        models.remove("trafo_xl_nt")
+        models.remove("trafo_xl")
+        print(">> ERROR: Gpus not able to handle sizes bigger than 10k. Octis is not able to use generators for data.")
+        return
+
+    models_copy = models.copy()
+
+    generate_bjobs(
+        topics=[2, 3, 5, 10, 20, 50, 100],
+        file_path="/cluster/work/cotterell/knobelf/",
+        data_path="/cluster/work/cotterell/knobelf/data/",
+        topic_models=["neural_lda"],
+        models=models,
+        models_copy=models_copy,
+        duals=["first", "second"],
+        sampling_methods=["multinomial", "top_p", "typ_p"],
+        samples=samples,
+        combis=["intersection", "union"],
+        time="4:00",
+        num_cpu=4,
+        cpu_mem=10240,
+        use_gpu=True,
+        num_gpus=1,
+        gpu_mem=10000,
+        gpu_model=None,     # NVIDIAGeForceRTX3090, NVIDIAGeForceGTX1080, NVIDIAGeForceGTX1080Ti, NVIDIAGeForceRTX2080Ti, NVIDIATITANRTX, QuadroRTX6000, TeslaV100_SXM2_32GB, NVIDIAA100_PCIE_40GB
+        num_var=10,
+        calc_var=False
+    )
+
+
+def generate_score_bjobs(samples=10000):
+    models = ["gpt2_nt", "gpt2", "trafo_xl_nt", "trafo_xl", "wiki_nt", "arxiv"]
+    if samples > 10000:
+        models.remove("trafo_xl_nt")
+        models.remove("trafo_xl")
+
+    models_copy = models.copy()
+
+    generate_bjobs(
+        topics=[1],
+        file_path="/cluster/work/cotterell/knobelf/",
+        data_path="/cluster/work/cotterell/knobelf/data/",
+        topic_models=["classic_lda"],
+        models=models,
+        models_copy=models_copy,
+        duals=["first"],
+        sampling_methods=["multinomial", "top_p", "typ_p"],
+        samples=samples,
+        combis=["intersection"],
+        time="24:00",
+        num_cpu=4,
+        cpu_mem=10240,
+        calc_score=True,
+        score_mode=["cv", "tt", "tp", "img"]
+    )
+
+
+def assemble_score():
+    pass
 
 
 def generate_score_plot():
@@ -374,9 +498,22 @@ def generate_plot_var_same():
 
 
 def main():
-    set_seed(42)
-    generate_score_plot()
+    gc.collect()
+    torch.cuda.empty_cache()
 
+    #generate_tokenize_bjobs(100000)
+    #generate_tokenize_bjobs(10000)
+    
+    #generate_classic_lda_bjobs(100000)
+    #generate_classic_lda_bjobs(10000)
+    
+    #generate_classic_lda_variation_bjobs(100000)
+    #generate_classic_lda_variation_bjobs(10000)
+
+    #generate_neural_lda_bjobs(10000)
+
+    generate_score_bjobs(10000)
+    generate_score_bjobs(100000)
 
 if __name__ == '__main__':
     main()
